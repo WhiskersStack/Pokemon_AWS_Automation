efe625cc684071661f129cd4ba6feff9707814a2
JenJen123123 <ssystem32@gmail.com>
2025-05-15 01:31:13 +0300

    setupV2

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import sys
+import venv
+from ec2_init import main
+
+
+def create_venv(venv_dir="venv"):
+    print("Creating virtual environment...")
+    venv.create(venv_dir, with_pip=True)
+
+
+def install_requirements(venv_dir="venv"):
+    print("Installing packages from requirements.txt...")
+    pip_path = os.path.join(venv_dir, "Scripts" if os.name == "nt" else "bin", "pip")
+    subprocess.check_call([pip_path, "install", "-r", "requirements.txt"])
+
+
+def run_main_script():
+    main()  # Call the main function from ec2_init.py
+
+
+def main():
+    create_venv()
+    install_requirements()
+    run_main_script()
+    print("\n✅ All done!")
+
+
+if __name__ == "__main__":
+    main()
